faab7c1c184a377fd692f5472e753b38fee7071d
Andrew Aleynikov <drew.lake@yandex.com>
2024-05-06 17:55:33 +0300

    Add first dummy test

--- lib/src/test/kotlin/adapters/JsonSwaggerSchemaV3AdapterTest.kt
+++ lib/src/test/kotlin/adapters/JsonSwaggerSchemaV3AdapterTest.kt
@@ -1,8 +1,21 @@
 package adapters
 
+import adapters.output.SwaggerSchemaOutputStream
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
 class JsonSwaggerSchemaV3AdapterTest {
 
-    fun test() {
+    private val output = mock(SwaggerSchemaOutputStream::class.java)
+    private val sut = JsonSwaggerSchemaV3Adapter(output)
 
+    @Test
+    fun `Output as is it is`() {
+        val value = "{}"
+
+        sut.convert(value)
+
+        verify(output).flush(value)
     }
 }
