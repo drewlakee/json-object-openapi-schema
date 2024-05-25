ee325b12fec9172e0898e393383e1a8b4640748e
Andrew Aleynikov <drew.lake@yandex.com>
2024-05-25 15:52:47 +0300

    Add schema name generator test-cases for while condition

--- lib/src/test/kotlin/io/github/jooas/adapters/openapi/yaml/SchemaNameGeneratorTest.kt
+++ lib/src/test/kotlin/io/github/jooas/adapters/openapi/yaml/SchemaNameGeneratorTest.kt
@@ -17,6 +17,17 @@ class SchemaNameGeneratorTest {
         assertEquals(expected, actual)
     }
 
+    @ParameterizedTest
+    @MethodSource("pascalCaseTestWhileCases")
+    fun `Convert pascal case with while condition`(
+        value: String,
+        expected: String,
+    ) {
+        var counter = 0
+        val actual = SchemaNameGenerator.tryPascalCaseWhile(value) { _ -> counter++ != 10 }
+        assertEquals(expected, actual)
+    }
+
     companion object {
         @JvmStatic
         fun pascalCaseTestCases(): Stream<Arguments> =
@@ -28,6 +39,18 @@ class SchemaNameGeneratorTest {
                 Arguments.of("1241", ""),
                 Arguments.of("89-/", ""),
                 Arguments.of("meta-information", "MetaInformation"),
+            )
+
+        @JvmStatic
+        fun pascalCaseTestWhileCases(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of("object_value", "ObjectValue10"),
+                Arguments.of("asd-1", "Asd10"),
+                Arguments.of("Object", "Object10"),
+                Arguments.of("Schema_2", "Schema10"),
+                Arguments.of("1241", "10"),
+                Arguments.of("89-/", "10"),
+                Arguments.of("meta-information", "MetaInformation10"),
             )
     }
 }
